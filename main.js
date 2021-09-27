4cf95ac5861353d5ec6a776e65b77b3a99b2885a
hugo <hugo.herrador@gmail.com>
2021-09-27 17:32:35 +0200

    webgl map setup

--- main.js
+++ main.js
@@ -1,25 +1,60 @@
-import 'ol/ol.css';
-import MVT from 'ol/format/MVT';
-import VectorTileLayer from 'ol/layer/VectorTile';
-import VectorTileSource from 'ol/source/VectorTile';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
 import {Map, View} from 'ol';
+import {Stamen, Vector as VectorSource} from 'ol/source';
 import {fromLonLat} from 'ol/proj';
 
-const map = new Map({
+const source = new VectorSource();
+
+const client = new XMLHttpRequest();
+client.open('GET', './data/meteorites.csv');
+client.onload = function () {
+  const csv = client.responseText;
+  const features = [];
+
+  let prevIndex = csv.indexOf('\n') + 1; // scan past the header line
+
+  let curIndex;
+  while ((curIndex = csv.indexOf('\n', prevIndex)) != -1) {
+    const line = csv.substr(prevIndex, curIndex - prevIndex).split(',');
+    prevIndex = curIndex + 1;
+
+    const coords = fromLonLat([parseFloat(line[4]), parseFloat(line[3])]);
+    if (isNaN(coords[0]) || isNaN(coords[1])) {
+      // guard against bad data
+      continue;
+    }
+
+    features.push(
+      new Feature({
+        mass: parseFloat(line[1]) || 0,
+        year: parseInt(line[2]) || 0,
+        geometry: new Point(coords),
+      })
+    );
+  }
+  source.addFeatures(features);
+};
+client.send();
+
+const meteorites = new VectorLayer({
+  source: source,
+});
+
+new Map({
   target: 'map-container',
+  layers: [
+    new TileLayer({
+      source: new Stamen({
+        layer: 'toner',
+      }),
+    }),
+    meteorites,
+  ],
   view: new View({
-    center: fromLonLat([0, 0]),
+    center: [0, 0],
     zoom: 2,
   }),
 });
-
-const layer = new VectorTileLayer({
-  source: new VectorTileSource({
-    format: new MVT(),
-    url:
-      'https://ahocevar.com/geoserver/gwc/service/tms/1.0.0/' +
-      'ne:ne_10m_admin_0_countries@EPSG%3A900913@pbf/{z}/{x}/{-y}.pbf',
-    maxZoom: 14,
-  }),
-});
-map.addLayer(layer);
